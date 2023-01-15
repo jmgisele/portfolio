bb9c2a1b8b7cf51b2efdf087d160d9d67d03aad7
James Gisele <james.may.gisele@protonmail.com>
2023-01-15 17:18:54 -0500

    adding new resume, new url for yelpcamp

--- client/src/components/right-col/projects-page/projects-page.component.jsx
+++ client/src/components/right-col/projects-page/projects-page.component.jsx
@@ -1,86 +1,140 @@
-import React from 'react'
+import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore, { Navigation, EffectCards } from "swiper";
 import "swiper/css";
 import "swiper/css/navigation";
-import "swiper/css/effect-cards"
+import "swiper/css/effect-cards";
 
-import { NavAnimation } from '../nav-animation/nav-animation.component';
-import { ColorfulBackground } from '../colorful-background/colorful-background.component';
+import { NavAnimation } from "../nav-animation/nav-animation.component";
+import { ColorfulBackground } from "../colorful-background/colorful-background.component";
 
-import campPic from '../../../assets/generic.avif'
-import todoPic from '../../../assets/desktop.avif'
-import './projects-page-styles.css'
+import campPic from "../../../assets/generic.avif";
+import todoPic from "../../../assets/desktop.avif";
+import "./projects-page-styles.css";
 
 // Swiper modules
 SwiperCore.use([EffectCards]);
 
-const ProjectCard = ({ link, title, pic, imgAltText, toolsUsed, codeLink,
-    projectDescription, knownBugs }) => (
-        <article className="project-card">
-            <img src={pic} alt={imgAltText} loading="lazy"></img>
-            <a href={link} className="project-title" target="_blank" rel="noreferrer">{title}</a>
-            <div className="tools-box">
-                <h3 className="tools-content">Languages + Tools Used</h3>
-                <div>{toolsUsed.map((tool) => <span className="tools-tag">{tool}</span>)}</div>
-            </div>
-            <a href={codeLink} className="code-link" target="_blank" rel="noreferrer">Github Link</a>
-            <div className="inner-content">
-                <h3>Project Description</h3>
-                <p>{projectDescription}</p>
-                <h4>Future Development & Known Bugs</h4>
-                <p>{knownBugs}</p>
-            </div>
-        </article>
-    )
-
+const ProjectCard = ({
+  link,
+  title,
+  pic,
+  imgAltText,
+  toolsUsed,
+  codeLink,
+  projectDescription,
+  knownBugs,
+}) => (
+  <article className="project-card">
+    <img src={pic} alt={imgAltText} loading="lazy"></img>
+    <a href={link} className="project-title" target="_blank" rel="noreferrer">
+      {title}
+    </a>
+    <div className="tools-box">
+      <h3 className="tools-content">Languages + Tools Used</h3>
+      <div>
+        {toolsUsed.map((tool) => (
+          <span className="tools-tag">{tool}</span>
+        ))}
+      </div>
+    </div>
+    <a href={codeLink} className="code-link" target="_blank" rel="noreferrer">
+      Github Link
+    </a>
+    <div className="inner-content">
+      <h3>Project Description</h3>
+      {projectDescription}
+      <h4>Future Development & Known Bugs</h4>
+      {knownBugs}
+    </div>
+  </article>
+);
 
 export const ProjectsPage = () => (
-    <NavAnimation id="flex-projects-container">
-        <ColorfulBackground>
-            <Swiper navigation={true} modules={[Navigation]} className="swiper-container">
-                <SwiperSlide>
-                    <ProjectCard
-                        link="https://fast-meadow-29979.herokuapp.com/campgrounds"
-                        title="YelpCamp"
-                        pic={campPic}
-                        imgAltText="campground at night, as seen in YelpCamp portfolio project"
-                        toolsUsed={['Node.js', 'MongoDB', 'Express', 'Bootstrap', 'Vanilla JS', 'EJS', 'Cloudfare', 'Mapbox']}
-                        codeLink="https://github.com/jmgisele/YelpCamp"
-                        projectDescription={<p>A crowd-sourcing campsite review board mock-up completed as part of a <span>
-                            <a href="https://www.udemy.com/course/the-web-developer-bootcamp/">Udemy full-stack course</a></span>.
-                            Implements full CRUD, authentication/authorization, basic sanitation of user inputs, and more. I
-                            also implemented custom error-handling and both client- and server-side validation to make sure
-                            nobody was uploading 1000s of images to my free cloudfare account. . . .</p>}
-                        knownBugs="An edit option for comments would be very easy to add. The main page loads all campsites at once, which isn't the 
-                            best visual experience; I might add in lazy loading at some point. I'd also love to add a more robust user panel
-                            where users can see all the campsites they've added, and perhaps a favoriting feature for users to collect others' campsites."
-                    />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <ProjectCard
-                        link="https://jmgisele.github.io/todo-app-main/"
-                        title="Todo App"
-                        pic={todoPic}
-                        imgAltText="screenshot of todo app interface"
-                        toolsUsed={['IndexedDB', 'Vanilla JS']}
-                        codeLink="https://github.com/jmgisele/todo-app-main"
-                        projectDescription={<p>The obligatory web dev todo app. I decided
-                            to add persistent storage to store the todos for an extra challenge.
-                            Storage is handled client-side using IndexedDB. I assumed
-                            working with IDB would be quicker than setting up a full back end and hosting it,
-                            but working with IDB is a bit of a pain. If I were remaking this project, I'd probably
-                            just whip up a quick Node/Express/Mongo backend with session that refreshed after a
-                            week or so and cleared their data from the database; users wouldn't have to
-                            log in, could still have semi-persistent storage using session, and the DB wouldn't be
-                            cluttered with old unused entries.</p>}
-                        knownBugs="Order of the todos doesn't persist on re-load. This could be implemented using a property on
-                            the IDB entry for each todo that kept track of its place in the page, maybe using on-drop
-                            handlers thru Sortablejs."
-                    />
-                </SwiperSlide>
-            </Swiper>
-        </ColorfulBackground>
-    </NavAnimation>
-
-)
+  <NavAnimation id="flex-projects-container">
+    <ColorfulBackground>
+      <Swiper
+        navigation={true}
+        modules={[Navigation]}
+        className="swiper-container"
+      >
+        <SwiperSlide>
+          <ProjectCard
+            link="https://yelpcamp-jamesgisele.fly.dev/"
+            title="YelpCamp"
+            pic={campPic}
+            imgAltText="campground at night, as seen in YelpCamp portfolio project"
+            toolsUsed={[
+              "Node.js",
+              "MongoDB",
+              "Express",
+              "Bootstrap",
+              "Vanilla JS",
+              "EJS",
+              "Cloudfare",
+              "Mapbox",
+            ]}
+            codeLink="https://github.com/jmgisele/YelpCamp"
+            projectDescription={
+              <p>
+                Baby's first CRUD app! I completed this crowd-sourcing campsite
+                review board mock-up as part of a{" "}
+                <span>
+                  <a href="https://www.udemy.com/course/the-web-developer-bootcamp/">
+                    Udemy full-stack course
+                  </a>
+                </span>
+                , in order to learn the basics of full stack web. Implements
+                full CRUD, authentication/authorization, basic sanitation of
+                user inputs, etc. I also added on custom error-handling and both
+                client- and server-side validation to make sure nobody was
+                uploading 1000s of images to my free cloudfare account. . . .
+              </p>
+            }
+            knownBugs={
+              <p>
+                An edit option for comments would be very easy to add. The main
+                page loads all campsites at once, which isn't the best visual
+                experience; lazy loading would be better. It's unlikely I'll
+                ever touch this project again; it was an excellent way to learn
+                the basics but isn't exactly glamorous.
+              </p>
+            }
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <ProjectCard
+            link="https://jmgisele.github.io/todo-app-main/"
+            title="Todo App"
+            pic={todoPic}
+            imgAltText="screenshot of todo app interface"
+            toolsUsed={["IndexedDB", "Vanilla JS"]}
+            codeLink="https://github.com/jmgisele/todo-app-main"
+            projectDescription={
+              <p>
+                The obligatory web dev todo app. I decided to add persistent
+                storage to store the todos for an extra challenge. Storage is
+                handled client-side using IndexedDB. I assumed working with IDB
+                would be quicker than setting up a full back end and hosting it,
+                but working with IDB is a bit of a pain. If I were remaking this
+                project, I'd probably just whip up a quick Node/Express/Mongo
+                backend with session that refreshed after a week or so and
+                cleared their data from the database; users wouldn't have to log
+                in, could still have semi-persistent storage using session, and
+                the DB wouldn't be cluttered with old unused entries.
+              </p>
+            }
+            knownBugs={
+              <p>
+                Order of the todos doesn't persist on re-load. This could be
+                implemented using a property on the IDB entry for each todo that
+                kept track of its place in the page, maybe using on-drop
+                handlers thru Sortablejs.
+              </p>
+            }
+          />
+        </SwiperSlide>
+      </Swiper>
+    </ColorfulBackground>
+  </NavAnimation>
+);
--- client/src/components/right-col/skills-page/skills-page.component.jsx
+++ client/src/components/right-col/skills-page/skills-page.component.jsx
@@ -1,35 +1,62 @@
-import React from 'react'
-import { NavAnimation } from '../nav-animation/nav-animation.component';
+import React from "react";
+import { NavAnimation } from "../nav-animation/nav-animation.component";
 
-import './skills-page-styles.css'
+import "./skills-page-styles.css";
 
-const SkillItems = ({items}) => {
-    const list = items.map((item, i) => <li key={i}>{item}</li>)
-    return list;
-}
+const SkillItems = ({ items }) => {
+  const list = items.map((item, i) => <li key={i}>{item}</li>);
+  return list;
+};
 
-
-const SkillCard = ({title, items}) => (
-    <div className="skill-card">
-        <h6>{title}</h6>
-        <ul>
-            <SkillItems items={items} />
-        </ul>
-    </div>
-)
-
+const SkillCard = ({ title, items }) => (
+  <div className="skill-card">
+    <h6>{title}</h6>
+    <ul>
+      <SkillItems items={items} />
+    </ul>
+  </div>
+);
 
 export const SkillsPage = () => (
-    <NavAnimation id="skills-nav-container">
-        <div id="skills-title-box">
-            <h1 id="skills-title">Languages, Skills, Tools, Frameworks</h1>
-            <h5 id="skills-subtitle">Don't see your stack? I'm always looking to pick up something new....</h5>
-        </div>
-        <section id="flex-skills-container">
-            <SkillCard title="Langauages, Frameworks, Libraries" items={['React/Next.js', 'Flask', 'Django', 'Node', 'TypeScript', '(legacy) ColdFusion', '(legacy) jQuery' ]} />
-            <SkillCard title="Styling" items={['SCSS', 'Bootstrap', 'MUI/Chakra', 'Figma']} />
-            <SkillCard title="Databases" items={['SQL Server', 'Postgres', 'MongoDB']} />
-            <SkillCard title="Tools, Testing, Etc" items={['CI/CD', 'Agile', 'Linux/Bash', 'Jest/Enzyme', 'React Testing Library', 'RESTful API design' ]} />
-        </section>
-    </NavAnimation>
-)
+  <NavAnimation id="skills-nav-container">
+    <div id="skills-title-box">
+      <h1 id="skills-title">Languages, Skills, Tools, Frameworks</h1>
+      <h5 id="skills-subtitle">
+        Don't see your stack? I'm always looking to pick up something new....
+      </h5>
+    </div>
+    <section id="flex-skills-container">
+      <SkillCard
+        title="Langauages, Frameworks, Libraries"
+        items={[
+          "React/Next.js",
+          "Svelte",
+          "Flask",
+          "Node, Express",
+          "TypeScript",
+          "(legacy) ColdFusion",
+          "(legacy) jQuery",
+        ]}
+      />
+      <SkillCard
+        title="Styling"
+        items={["SCSS", "Bootstrap", "MUI/Chakra", "Figma"]}
+      />
+      <SkillCard
+        title="Databases"
+        items={["SQL Server", "Postgres", "MongoDB"]}
+      />
+      <SkillCard
+        title="Tools, Testing, Etc"
+        items={[
+          "CI/CD",
+          "Agile",
+          "Linux/Bash",
+          "Jest/Enzyme",
+          "React Testing Library",
+          "RESTful API design",
+        ]}
+      />
+    </section>
+  </NavAnimation>
+);
