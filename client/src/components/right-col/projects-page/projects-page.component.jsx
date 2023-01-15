8c013e3df05e87c793876aa8d7e66356b25a8573
James Gisele <james.may.gisele@protonmail.com>
2023-01-15 17:19:21 -0500

    adding new resume, new url for yelpcamp

--- client/src/components/right-col/projects-page/projects-page.component.jsx
+++ client/src/components/right-col/projects-page/projects-page.component.jsx
@@ -97,7 +97,7 @@ export const ProjectsPage = () => (
                 page loads all campsites at once, which isn't the best visual
                 experience; lazy loading would be better. It's unlikely I'll
                 ever touch this project again; it was an excellent way to learn
-                the basics but isn't exactly glamorous.
+                the basics but isn't exactly the most exciting app.
               </p>
             }
           />
